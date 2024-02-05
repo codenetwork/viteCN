7b3378c9f94e2f382c7401de76c3bf679e01580d
Patrick <42367024+Patadam@users.noreply.github.com>
2024-02-05 11:57:48 +1000

    fix(title): auto append | code network to title

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 export default function Page(props) {
   // Change title if a 'title' prop is passed
   useEffect(() => {
-    if (props.title) document.title = props.title;
+    if (props.title) document.title = `${props.title} | Code Network`;
   }, []);
   return (
     <div class={"p-6 md:px-12 md:py-16 h-100 max-w-screen-2xl mx-auto overflow-x-hidden 2xl:overflow-x-visible	 " + " " + props.class}>
