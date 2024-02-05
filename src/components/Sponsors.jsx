9bc95e385379af330580f816b791ada5d20535d9
Patrick <42367024+Patadam@users.noreply.github.com>
2024-02-05 13:15:21 +1000

    fix: show label when sponsor level exists

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -5,28 +5,28 @@ export default function Sponsors() {
   return (
     <>
       {/* PLATINUM */}
-      {sponsors?.Platinum ? <><h3 class="text-3xl text-gray-400 mt-10">Platinum Sponsor{sponsors?.Platinum?.length>1 ? "s" : ""}</h3>
+      {sponsors?.Platinum.length > 0  ? <><h3 class="text-3xl text-gray-400 mt-10">Platinum Sponsor{sponsors?.Platinum?.length>1 ? "s" : ""}</h3>
       <div class="flex flex-row justify-center mt-8 gap-12 flex-wrap">
         {sponsors.Platinum.map((company, index) => (
           <Sponsor key={index} company={company} />
         ))}
       </div></> : ""}
       {/* GOLD */}
-      {sponsors?.Gold ? <><h3 class="text-3xl text-gray-400 mt-10">Gold Sponsor{sponsors?.Gold?.length>1 ? "s" : ""}</h3>
+      {sponsors?.Gold.length > 0  ? <><h3 class="text-3xl text-gray-400 mt-10">Gold Sponsor{sponsors?.Gold?.length>1 ? "s" : ""}</h3>
       <div class="flex flex-row justify-center mt-8 gap-12 flex-wrap">
         {sponsors.Gold.map((company, index) => (
           <Sponsor key={index} company={company} />
         ))}
       </div></> : ""}
       {/* SILVER */}
-      {sponsors?.Silver ? <><h3 class="text-3xl text-gray-400 mt-10">Silver Sponsor{sponsors?.Silver?.length>1 ? "s" : ""}</h3>
+      {sponsors?.Silver.length > 0 ? <><h3 class="text-3xl text-gray-400 mt-10">Silver Sponsor{sponsors?.Silver?.length>1 ? "s" : ""}</h3>
       <div class="flex flex-row justify-center mt-8 gap-12 flex-wrap">
         {sponsors.Silver.map((company, index) => (
           <Sponsor key={index} company={company} />
         ))}
       </div></> : ""}
       {/* BRONZE */}
-      {sponsors?.Bronze ? <><h3 class="text-3xl text-gray-400 mt-10">Bronze Sponsor{sponsors?.Bronze?.length>1 ? "s" : ""}</h3>
+      {sponsors?.Bronze.length > 0  ? <><h3 class="text-3xl text-gray-400 mt-10">Bronze Sponsor{sponsors?.Bronze?.length>1 ? "s" : ""}</h3>
       <div class="flex flex-row justify-center mt-8 gap-12 flex-wrap">
         {sponsors.Bronze.map((company, index) => (
           <Sponsor key={index} company={company} />
