6ec1efdff00c8475978886268398dc4723b6824c
Patrick <42367024+Patadam@users.noreply.github.com>
2024-03-22 14:37:20 +1000

    fix: mobile responsive rules

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -1,30 +1,53 @@
-import { Button, Link, Stack } from "@mui/material";
 import Page from "../components/Page";
-import { H1 } from "../components/Text";
-import BasicTabs from "../components/RulesTabs"
+import { useState } from "react";
+
+import GeneralRules from "./rules/General";
+import Conduct from "./rules/Conduct";
+import SafeSpace from "./rules/SafeSpace";
+import Constitution from "./rules/Constitution";
+
+export function Tab(props) {
+  return (
+    <li>
+      <input type="radio" id={props.id} name="rules" value={props.value} className="hidden peer" />
+      <label
+        for={props.for}
+        className="inline-flex items-center justify-between w-full pb-1 transition cursor-pointer border-2 py-1 px-3  peer-checked:border-b-2 peer-checked:border-primary ">
+        <div className="w-full text-lg">{props.children}</div>
+      </label>
+    </li>
+  );
+}
 
 export default function Rules() {
-  return (
-    <Page title="Rules">  
-      {/* <H1>Code Network Rules and Policies</H1>
-      <p>Code Network aims to provide a safe, educational and fun environment for all its members.</p> */}
-      {/* <p>To achieve this we ask that the following rules be applied when engaging with the club or its members.</p>
-      <div class="my-6 flex flex-row gap-4 flex-wrap">
-        <Button variant="outlined" color="primary" href="/rules/general">
-          General Rules
-        </Button>
-        <Button variant="outlined" color="primary" href="/rules/conduct">
-          Code of Conduct
-        </Button>
-        <Button variant="outlined" color="primary" href="/rules/safespace">
-          Safe Space Policy
-        </Button>
-        <Button variant="outlined" color="primary" href="/constitution">
-          Constitution
-        </Button>
-      </div> */}
+  const [selected, setSelected] = useState("");
 
-      <BasicTabs />
+  return (
+    <Page title="Rules">
+      <h1 className="text-2xl pb-4">Our Rules</h1>
+      <form onChange={(e)=>setSelected(e.target.value)}>
+        <ul className="flex flex-row flex-wrap gap-4 py-2">
+          <Tab id="general" for="general" value="general" callback={setSelected}>
+            General Rules
+          </Tab>
+          <Tab id="coc" for="coc" value="coc">
+            Code of Conduct
+          </Tab>
+          <Tab id="ssp" for="ssp" value="ssp">
+            Safe Space Policy
+          </Tab>
+          <Tab id="constitution" for="constitution" value="constitution">
+            Constitution
+          </Tab>
+        </ul>
+      </form>
+      <div className="pt-6">
+        {selected == "general" ? <GeneralRules /> : ""}
+        {selected == "" ? <GeneralRules /> : ""}
+        {selected == "coc" ? <Conduct /> : ""}
+        {selected == "ssp" ? <SafeSpace /> : ""}
+        {selected == "constitution" ? <Constitution /> : ""}
+      </div>
     </Page>
   );
 }
