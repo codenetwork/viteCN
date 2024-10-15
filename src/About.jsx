00ab3ab343a623228a2a93c15fc428e874eba62f
Corey McCormick <57275965+McCorsa@users.noreply.github.com>
2024-10-15 21:25:11 +1000

    Comment out team temporarily from about page

--- src/About.jsx
+++ src/About.jsx
@@ -31,8 +31,8 @@ function About() {
           </p>
           <div className='padding'></div>
           <h2 style={{marginBottom: 0}}>Our Team</h2>
-          <div style={{fontSize: "1.2rem", fontWeight: 'bold', textAlign: 'center'}}>Full team details coming soon!</div>
-          <div className="team-section">
+          <div style={{fontSize: "1.2rem", fontWeight: 'bold', textAlign: 'center'}}>2025 team details coming soon!</div>
+          {/* <div className="team-section">
             {teamData.map((member, index) => (
               <div className="team-member" key={index}>
                 <img src={member.photo} alt={member.name} />
@@ -46,7 +46,7 @@ function About() {
                 </p>
               </div>
             ))}
-          </div>
+          </div> */}
         </div>
       </div >
     </div>
