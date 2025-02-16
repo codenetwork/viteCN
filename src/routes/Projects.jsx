01a3eff8e1dc9b5cd495f047e9a66b05bc04fb0a
Corey McCormick <coreymccor@live.com.au>
2025-02-16 15:37:33 +1000

    Add a whole bunch of details to the projects page

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,10 +1,40 @@
 import { Client } from "@notionhq/client"
 import { useEffect, useState } from "react"
+import "../styles/projects.css"
+// /** @jsxImportSource @emotion/react */
+// import { css } from "@emotion/react"
 
 const notion = new Client({
   auth: import.meta.env.VITE_NOTION_KEY,
   baseUrl: location.origin + "/api"
 })
+
+// Source: https://notioneers.eu/en/insights/notion-colors-codes
+const notionBgColorMap = {
+  "red": "#BE524B",
+  "pink": "#BA4A78",
+  "purple": "#865DBB",
+  "blue": "#447ACB",
+  "green": "#4F9768",
+  "yellow": "#C19138",
+  "orange": "#CB7B37",
+  "brown": "#A27763",
+  "gray": "#9B9B9B",
+  "default": "#D4D4D4"
+}
+
+const notionFgColorMap = {
+  "red": "#332523",
+  "pink": "#2E2328",
+  "purple": "#2A2430",
+  "blue": "#1F282D",
+  "green": "#242B26",
+  "yellow": "#372E20",
+  "orange": "#36291F",
+  "brown": "#2E2724",
+  "gray": "#252525",
+  "default": "#191919"
+}
 
 export default function ProjectsPage() {
   const [projects, setProjects] = useState();
@@ -33,9 +63,20 @@ export default function ProjectsPage() {
         <h1>Code Network Projects</h1>
         <p>Looking to flex your coding muscles? Help us build the most awesome projects!</p>
         <h2>Our Current Projects</h2>
-        {projects ? <ul>
-          {projects.map(project => <li key={project.id}>{project.properties.Name.title[0].text.content} <a href={project.properties["GitHub Repo"].url}>({project.properties["GitHub Repo"].url})</a></li>)}  
-        </ul> : <p>Loading our current projects...</p>}
+        {projects ? <div style={{ display: "grid", gridTemplateColumns: "auto auto auto", gap: "0.5em" }}>
+          {projects.map(project =>
+            <a
+              className="project-card"
+              href={project.properties["GitHub Repo"].url}
+              target="_blank"
+            >
+              <span style={{ fontSize: "2em" }}>{project.icon?.emoji}</span>
+              <span>{project.properties.Name.title[0].text.content}</span>
+              <span style={{ display: "flex", gap: "0.25em", fontSize: "0.75em" }}>{project.properties.Tags.multi_select.map(tag =>
+                <span class="project-badge" style={{ backgroundColor: notionBgColorMap[tag.color], color: notionFgColorMap[tag.Color], padding: "0.25em", borderRadius: "0.25em" }}>{tag.name}</span>
+              )}</span>
+            </a>)}
+        </div> : <p>Loading our current projects...</p>}
       </div>
     </main>
   )
