a2d00139aba2248010cc5fc32011eaac7f525d0d
Corey McCormick <coreymccor@live.com.au>
2025-05-20 17:06:31 +1000

    Improvements to home page and navbar links

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,6 +3,7 @@ import codeNetworkLogo from "../assets/codeNetworkLogoClear.png";
 import { FaBars, FaTimes } from "react-icons/fa";
 import "./Navbar.css";
 import SocialDropdown from "./SocialDropdown";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const navRef = useRef();
@@ -31,20 +32,21 @@ function Navbar() {
     <header className={isScrolled ? "scrolled" : ""}>
       <div className={`edges2 ${isScrolled ? "scrolled" : ""}`}>
         <h3>
-          <a href="/">
+          <Link to="/">
             <img src={codeNetworkLogo} alt="Logo of Code Network" height={60} />
-          </a>
+          </Link>
         </h3>
         <nav ref={navRef}>
-          <a href="/about">About us</a>
+          <Link to="/about">About us</Link>
           <div className="dropdown">
             <div className="dropdown-content">
               <SocialDropdown />
             </div>
           </div>
-          <a href="/rules">Rules</a>
-          <a href="/projects">Projects</a>
-          <a href="/competition">✨ Design Competition ✨</a>
+          <Link to="/rules">Rules</Link>
+          <Link to="/projects">Projects</Link>
+          {/* <Link to="/competition">Design Competition</Link> */}
+          <Link to="/sgm">Special General Meeting</Link>
 
           <button className="nav-btn nav-close-btn" onClick={showNavbar}>
             <FaTimes />
