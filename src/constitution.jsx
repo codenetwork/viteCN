7e1fefac847747611dea147b6678984ad5acc3ab
Ethan Burnet <ethanandrewburnet@gmail.com>
2026-03-05 20:35:46 +1000

    Updated constitution from March 2nd SGM

--- src/constitution.jsx
+++ src/constitution.jsx
@@ -3,10 +3,10 @@ function Constitution() {
   return (
     <div className="edges">
       <h1>Code Network Constitution</h1>
-      <p>This version was adopted at the 2024 Annual General Meeting of Code Network. Future revisions will appear here.</p>
+      <p>This version was adopted at the Special General Meeting of Code Network on March 2nd, 2026. Future revisions will appear here.</p>
       <object
         type="application/pdf"
-        data="/constitution_2024.pdf" // be sure to update this
+        data="/constitution_2026.pdf" // be sure to update this
         width="100%"
         height="600"
       />
